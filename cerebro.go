46541ca56d64d436f95f4647f0a630a1db0caf72
Sebastian Alvarez <sebalvareztto@gmail.com>
2020-12-25 16:37:31 -0300

    fix: functioning things up a little bit

--- cerebro.go
+++ cerebro.go
@@ -20,8 +20,8 @@ func checkNext(x int, v byte, row []byte) int {
 	if y >= len(row) {
 		return 0
 	}
-	fmt.Printf("\nCheckRight in %v|%v: \n", x, y)
-	fmt.Printf("Comparing : %v to %v \n", v, (row)[y])
+	//fmt.Printf("\nCheckRight in %v|%v: \n", x, y)
+	//fmt.Printf("Comparing : %v to %v \n", v, (row)[y])
 	if v == (row)[y] {
 		cn := checkNext(y, (row)[y], row)
 		if cn <= 3 {
@@ -42,8 +42,8 @@ func checkDiagonal(x int, y int, v byte, a [][]byte) int {
 		return 0
 	}
 	vi := row[yi]
-	fmt.Printf("\nCheckDiagonal in %v|%v: \n", x, y)
-	fmt.Printf("Comparing : %v to %v \n", v, vi)
+	//fmt.Printf("\nCheckDiagonal in %v|%v: \n", x, y)
+	//fmt.Printf("Comparing : %v to %v \n", v, vi)
 	if v == vi {
 		cd := checkDiagonal(xi, yi, vi, a)
 		if cd <= 3 {
@@ -53,40 +53,25 @@ func checkDiagonal(x int, y int, v byte, a [][]byte) int {
 	return 0
 }
 
-func main() {
-
-	fmt.Println("Hello, I'm cerebro.")
-
-	input := [...]string{"ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"}
-
-	fmt.Printf("Printing input: %+q \n", input)
-
-	dna := make([][]byte, len(input))
-	fmt.Printf("%T\n\n", dna)
-	for i, v := range input {
-		dna[i] = []byte(v)
-	}
-
-	fmt.Printf("Printing nitrogen bases for dna %v \n", dna)
+func isMutant(dna [][]byte) bool {
+	//fmt.Printf("Printing nitrogen bases for dna %v \n", dna)
 	and := transpose(dna)
-
-	fmt.Printf("Printing nitrogen bases for and %v \n", and)
-
+	//fmt.Printf("Printing nitrogen bases for and %v \n", and)
 	ms := 0
-
-	//validate that len(dna) == len(and)
-
+	if len(dna) != len(and) {
+		return false
+	}
 	for ri := 0; ri < len(dna); ri++ {
 		if ms > 1 {
 			break
 		}
 		row1 := dna[ri]
 		row2 := and[ri]
-		fmt.Printf("\nRows %v : %+q - %v | %+q - %v \n", ri, row1, row1, row2, row2)
+		//fmt.Printf("\nRows %v : %+q - %v | %+q - %v \n", ri, row1, row1, row2, row2)
 		for ci := 0; ci < len(row1); ci++ {
 			sr1 := row1[ci:]
 			sr2 := row2[ci:]
-			fmt.Printf("Subrows: %v | %v \n", sr1, sr2)
+			//fmt.Printf("Subrows: %v | %v \n", sr1, sr2)
 			result1 := checkNext(ci, row1[ci], sr1)
 			result2 := checkNext(ci, row2[ci], sr2)
 			result3 := checkDiagonal(ri, ci, row1[ci], dna)
@@ -99,29 +84,33 @@ func main() {
 			if result3 >= 3 {
 				ms++
 			}
-			fmt.Printf("Results of checking: %v | %v | %v \n", result1, result2, result3)
+			//fmt.Printf("Results of checking: %v | %v | %v \n", result1, result2, result3)
 		}
 
 	}
-
-	/*
-		for ri, row := range dna {
-				if ms > 1 {
-					break
-				}
-				fmt.Printf("\nRow %v : %+q - %v \n", ri, row, row)
-				for ci, nb := range row {
-					sr := row[ci:]
-					fmt.Printf("Subrow: %v \n", sr)
-					result := checkNext(ci, nb, sr)
-					if result >= 3 {
-						ms++
-					}
-					fmt.Printf("Result of checking: %v \n", result)
-				}
-			}
-	*/
-
 	fmt.Printf("\nMutant sequences: %v \n", ms)
+	return ms > 1
+}
+
+func main() {
+
+	fmt.Println("Hello, I'm cerebro.")
+
+	input := [...]string{"ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"}
+
+	//fmt.Printf("Printing input: %+q \n", input)
+
+	dna := make([][]byte, len(input))
+	//fmt.Printf("%T\n\n", dna)
+	for i, v := range input {
+		dna[i] = []byte(v)
+	}
+
+	im := isMutant(dna)
+	if im {
+		fmt.Println("I found a mutant!!")
+	} else {
+		fmt.Println("No luck this time...")
+	}
 
 }
