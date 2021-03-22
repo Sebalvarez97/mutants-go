7e36c31247027f291e5253ae7e5b91eaa68ed1c7
Sebastian Alvarez <sebastian.salvarez@mercadolibre.com>
2021-03-22 14:39:09 -0300

    tool not needed

--- internal/http/handlers.go
+++ internal/http/handlers.go
@@ -1,21 +1,9 @@
 package http
 
 import (
-	"github.com/Sebalvarez97/mutants-go/tools/web"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 type RouterHandler interface {
 	RouteURLs(r *gin.Engine)
-}
-
-func Response(w http.ResponseWriter, method string, body interface{}, err error) error {
-	if err != nil {
-		return err
-	}
-	if method == http.MethodPost {
-		return web.RespondJSON(w, body, http.StatusCreated)
-	}
-	return web.RespondJSON(w, body, http.StatusOK)
 }
